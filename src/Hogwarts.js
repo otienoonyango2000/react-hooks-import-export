abcc68c4fc40c4fcdc419e6e4a3dd19de3946dc8
otienoonyango2000 <otienoonyango2000@gmail.com>
2023-01-12 15:03:43 +0300

    import-export completed

---
+++ src/Hogwarts.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Hufflepuff from "./houses/Hufflepuff";
+
+function Hogwarts() {
+  return (
+    <div>
+      <Hufflepuff />
+    </div>
+  );
+}
+
+export default Hogwarts;
